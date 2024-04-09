72babfab4bac58c13c310939c5f5861e4838d353
Kevin Cipric <kc028@hdm-stuttgart.de>
2024-04-09 18:45:56 +0200

    implemented corresponding methods in repository and service classes, added testing, added .run.xml for TestService.java (won't run without specified vm options) #6

--- src/main/java/de/hdm_stuttgart/mi/dad/core/Service.java
+++ src/main/java/de/hdm_stuttgart/mi/dad/core/Service.java
@@ -2,16 +2,60 @@ package de.hdm_stuttgart.mi.dad.core;
 
 import de.hdm_stuttgart.mi.dad.ports.RepositoryPort;
 import de.hdm_stuttgart.mi.dad.ports.ServicePort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Service implements ServicePort {
 
     final RepositoryPort repository;
 
+    final Logger log = LoggerFactory.getLogger(Service.class);
+
     public Service(final RepositoryPort repository) {
         this.repository = repository;
     }
 
-    public void handle() {
-        repository.findInColumn();
+    @Override
+    public Map<String, List<List<String>>> searchTable(final List<String> tableNames, final Pattern pattern) throws Exception {
+
+        log.debug("table names: {}, search pattern: {}", tableNames, pattern.pattern());
+
+        final Map<String, List<List<String>>> results = HashMap.newHashMap(tableNames.size());
+
+        for (final String tableName : tableNames) {
+
+            log.debug("table name: {}", tableName);
+
+            final List<String> columnNames;
+            try {
+                columnNames = repository.findTableColumns(tableName);
+            } catch (final SQLException e) {
+                throw new SQLException("failed to find column names of table: " + e.getMessage(), e);
+            }
+
+            log.debug("column names of table: {}", columnNames);
+
+            final List<List<String>> rows;
+            try {
+                rows = repository.findInTable(tableName, columnNames, pattern);
+
+            } catch (final SQLException e) {
+                throw new SQLException("failed to find matching pattern in table rows: " + e.getMessage(), e);
+            }
+
+            log.debug("found rows in table: {}", rows);
+
+            results.put(tableName, rows);
+        }
+
+        log.debug("found table results: {}", results);
+
+        return results;
     }
 }
