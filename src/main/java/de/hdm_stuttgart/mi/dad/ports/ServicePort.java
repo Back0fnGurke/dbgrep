01862d9427defce4242045e824803ca444c43acd
Kevin Cipric <kc028@hdm-stuttgart.de>
2024-05-08 10:49:54 +0200

    changed comment #14

--- src/main/java/de/hdm_stuttgart/mi/dad/ports/ServicePort.java
+++ src/main/java/de/hdm_stuttgart/mi/dad/ports/ServicePort.java
@@ -15,7 +15,7 @@ public interface ServicePort {
      * Find rows in provided tables that have column values which match the provided search pattern.
      *
      * @param tableNames the table names to search in
-     * @param pattern    the pattern to search for in the column values
+     * @param property   the property to search for in the column values
      * @return a List of Tables. List of Rows in Table is Empty if none where found.
      * @throws SQLException if a database access error occurs
      */
