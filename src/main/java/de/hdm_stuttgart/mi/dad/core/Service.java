f8b7ba16353b02804089c16bfcb05a18f1b32a73
Kevin Cipric <kc028@hdm-stuttgart.de>
2024-05-08 10:58:30 +0200

    comment out service logic and test #14

--- src/main/java/de/hdm_stuttgart/mi/dad/core/Service.java
+++ src/main/java/de/hdm_stuttgart/mi/dad/core/Service.java
@@ -1,15 +1,14 @@
 package de.hdm_stuttgart.mi.dad.core;
 
 import de.hdm_stuttgart.mi.dad.core.entity.Table;
+import de.hdm_stuttgart.mi.dad.core.property.Property;
 import de.hdm_stuttgart.mi.dad.ports.RepositoryPort;
 import de.hdm_stuttgart.mi.dad.ports.ServicePort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class Service implements ServicePort {
 
@@ -22,39 +21,40 @@ public class Service implements ServicePort {
     }
 
     @Override
-    public List<Table> searchTables(final List<String> tableNames, final Pattern pattern) throws SQLException {
+    public List<Table> searchTables(final List<String> tableNames, final Property property) throws SQLException {
 
-        log.debug("table names: {}, search pattern: {}", tableNames, pattern.pattern());
-
-        final List<Table> results = new ArrayList<>(tableNames.size());
-
-        for (final String tableName : tableNames) {
-
-            log.debug("table name: {}", tableName);
-
-            final List<String> columnNames;
-            try {
-                columnNames = repository.findTableColumnNamesAll(tableName);
-            } catch (final SQLException e) {
-                throw new SQLException("failed to find column names of table: " + e.getMessage(), e);
-            }
-
-            log.debug("column names of table: {}", columnNames);
-
-            final Table table;
-            try {
-                table = repository.findPattern(tableName, columnNames, pattern);
-            } catch (final SQLException e) {
-                throw new SQLException("failed to find matching pattern in table rows: " + e.getMessage(), e);
-            }
-
-            log.debug("found rows in table: {}", table);
-
-            results.add(table);
-        }
-
-        log.debug("found table results: {}", results);
-
-        return results;
+//        log.debug("table names: {}, search pattern: {}", tableNames, pattern.pattern());
+//
+//        final List<Table> results = new ArrayList<>(tableNames.size());
+//
+//        for (final String tableName : tableNames) {
+//
+//            log.debug("table name: {}", tableName);
+//
+//            final List<String> columnNames;
+//            try {
+//                columnNames = repository.findTableColumnNamesAll(tableName);
+//            } catch (final SQLException e) {
+//                throw new SQLException("failed to find column names of table: " + e.getMessage(), e);
+//            }
+//
+//            log.debug("column names of table: {}", columnNames);
+//
+//            final Table table;
+//            try {
+//                table = repository.findPattern(tableName, columnNames, pattern);
+//            } catch (final SQLException e) {
+//                throw new SQLException("failed to find matching pattern in table rows: " + e.getMessage(), e);
+//            }
+//
+//            log.debug("found rows in table: {}", table);
+//
+//            results.add(table);
+//        }
+//
+//        log.debug("found table results: {}", results);
+//
+//        return results;
+        return List.of();
     }
 }
