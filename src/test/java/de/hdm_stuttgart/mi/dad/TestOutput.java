91f908de934c9ffaeb5947448d132fb4a03fced5
Alessa Wiesner <alessa56767@web.de>
2024-06-25 11:31:39 +0200

    Output Test mit Input

--- src/test/java/de/hdm_stuttgart/mi/dad/TestOutput.java
+++ src/test/java/de/hdm_stuttgart/mi/dad/TestOutput.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
@@ -32,6 +33,11 @@ public class TestOutput {
     @AfterEach
     public void tearDown() {
         System.setOut(standardOut);
+    }
+
+    void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
     }
 
     @Test
@@ -122,6 +128,117 @@ public class TestOutput {
                 + "9  | Aisha                             | 22  | " + System.lineSeparator()
                         + "-----------------------------------------------" + System.lineSeparator()
                 + "10 | Musa                              | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------";
+
+        assertEquals(exspected, outputStreamCaptor.toString()
+                .trim());
+    }
+
+    @Test
+    public void testOutputwithInteraction() {
+
+        OutputHandler outputHandler = new OutputHandler();
+        Table table = new Table("Test", Arrays.asList(
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "1"),
+                        new ColumnValue("Name", "Harry"),
+                        new ColumnValue("Age", "30")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "2"),
+                        new ColumnValue("Name", "Barry"),
+                        new ColumnValue("Age", "25")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "3"),
+                        new ColumnValue("Name", "Sarah-Jane Lillian Long Long Long"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "4"),
+                        new ColumnValue("Name", "Matt"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "5"),
+                        new ColumnValue("Name", "Xanxia"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "6"),
+                        new ColumnValue("Name", "Kate "),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "7"),
+                        new ColumnValue("Name", "Flora"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "8"),
+                        new ColumnValue("Name", "Bloom"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "9"),
+                        new ColumnValue("Name", "Aisha"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "10"),
+                        new ColumnValue("Name", "Musa"),
+                        new ColumnValue("Age", "22")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "11"),
+                        new ColumnValue("Name", "Suthek"),
+                        new ColumnValue("Age", "99")
+                )),
+                new Row(Arrays.asList(
+                        new ColumnValue("ID", "12"),
+                        new ColumnValue("Name", "Anubis"),
+                        new ColumnValue("Age", "98")
+                ))
+
+        ));
+
+        List<Property> properties = List.of(
+                PropertyFactory.getProperty(LIKE, Pattern.compile("Harry"))
+        );
+
+        provideInput("m");
+        outputHandler.printTable(table, properties);
+
+        String exspected =
+                "-----------------------------------------------" + System.lineSeparator()
+                        + "ID | Name                              | Age | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "1  | \u001B[31mHarry\u001b[0m                             | 30  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "2  | Barry                             | 25  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "3  | Sarah-Jane Lillian Long Long Long | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "4  | Matt                              | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "5  | Xanxia                            | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "6  | Kate                              | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "7  | Flora                             | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "8  | Bloom                             | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "9  | Aisha                             | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "10 | Musa                              | 22  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "Type m for more results. Type q to quit program." + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "11 | Suthek                            | 99  | " + System.lineSeparator()
+                        + "-----------------------------------------------" + System.lineSeparator()
+                        + "12 | Anubis                            | 98  | " + System.lineSeparator()
                         + "-----------------------------------------------";
 
         assertEquals(exspected, outputStreamCaptor.toString()
