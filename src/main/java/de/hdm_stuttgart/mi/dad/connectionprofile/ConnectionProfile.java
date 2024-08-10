7d75ab81c589263e6ebe6ac121dd1cb49e77e6ac
Cipric Kevin <kc028@hdm-stuttgart.de>
2024-08-10 19:22:37 +0200

    changed class ConnectionProfile to record

--- src/main/java/de/hdm_stuttgart/mi/dad/connectionprofile/ConnectionProfile.java
+++ src/main/java/de/hdm_stuttgart/mi/dad/connectionprofile/ConnectionProfile.java
@@ -3,45 +3,5 @@ package de.hdm_stuttgart.mi.dad.connectionprofile;
 /**
  * Store connection profile data.
  */
-public class ConnectionProfile {
-    private final String driver;
-    private final String host;
-    private final String port;
-    private final String user;
-    private final String password;
-    private final String database;
-
-    public ConnectionProfile(String driver, String host, String port, String user, String password, String dbName) {
-        this.driver = driver;
-        this.host = host;
-        this.user = user;
-        this.password = password;
-        this.database = dbName;
-        this.port = port;
-    }
-
-    public String getDriver() {
-        return driver;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getDatabase() {
-        return database;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
+public record ConnectionProfile(String driver, String host, String port, String user, String password, String database) {
 }
